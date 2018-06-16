47e963997d6c49c1e69d03dd88a68c4603428253
Tony <buddy90_lost@yahoo.co.id>
2018-06-16 17:12:37 +0700

    change constructor in Coordinate class

--- src/com/data/Coordinate.java
+++ src/com/data/Coordinate.java
@@ -1,11 +1,11 @@
 package com.data;
 
 public class Coordinate {
-    private String lon;
     private String lat;
-    Coordinate(String _lon, String _lat){
-        lon = _lon;
+    private String lon;
+    public Coordinate(String _lat, String _lon){
         lat = _lat;
+        lon = _lon;
     }
     String longitude(){
         return lon;
