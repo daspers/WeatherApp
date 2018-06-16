029967ead46994cd074f35864f921d3289854988
Tony <buddy90_lost@yahoo.co.id>
2018-06-16 19:55:51 +0700

    add copy constructor like

--- src/com/data/Coordinate.java
+++ src/com/data/Coordinate.java
@@ -1,16 +1,19 @@
 package com.data;
 
 public class Coordinate {
-    private String lat;
-    private String lon;
-    public Coordinate(String _lat, String _lon){
-        lat = _lat;
-        lon = _lon;
-    }
-    String longitude(){
-        return lon;
-    }
-    String latitude(){
-        return lat;
-    }
+  private String lat;
+  private String lon;
+  public Coordinate(String _lat, String _lon){
+    lat = _lat;
+    lon = _lon;
+  }
+  public Coordinate(Coordinate other){
+    this(other.latitude(), other.longitude());
+  }
+  String longitude(){
+    return lon;
+  }
+  String latitude(){
+    return lat;
+  }
 }
