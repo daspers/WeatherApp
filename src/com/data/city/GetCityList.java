a26f631746a14fe8b1c7b69f6f1f5a5e96b0c33e
Tony <buddy90_lost@yahoo.co.id>
2018-06-21 19:49:22 +0700

    add class for getting city list

---
+++ src/com/data/city/GetCityList.java
@@ -0,0 +1,44 @@
+package com.data.city;
+
+import com.functions.Functions;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
+public class GetCityList {
+  static private GetCityList dummy = null;
+  static private City[] citylist = null;
+  private GetCityList(){
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Tony\\Documents\\GitHub\\WeatherApp\\src\\com\\data\\city\\citylist.json"));
+      Gson data = new Gson();
+      citylist = data.fromJson(reader, City[].class);
+      for(City city : citylist){
+        city.setName(city.getName().toUpperCase());
+      }
+    }
+    catch (IOException e){
+      e.printStackTrace();
+      citylist = null;
+    }
+  }
+  private static void init(){
+    if(dummy == null)
+      dummy = new GetCityList();
+  }
+  public static ArrayList<City> byCityName(String cityname){
+    init();
+    ArrayList<City> result = new ArrayList<City>();
+    cityname = cityname.toUpperCase();
+    for(City city : citylist){
+      if(city.getName().contains(cityname)) {
+        result.add(new City(city));
+      }
+    }
+    return result;
+  }
+}
