7db87a908421ff029c8815731cf0d8c78d7ccb8c
Tony <buddy90_lost@yahoo.co.id>
2018-07-01 22:06:25 +0700

    update GetCityList class

--- src/com/data/city/GetCityList.java
+++ src/com/data/city/GetCityList.java
@@ -10,32 +10,25 @@ import java.util.Comparator;
 
 
 public class GetCityList {
-  static private GetCityList dummy = null;
   static private City[] citylist = null;
-  private GetCityList(){
-    try {
+  private GetCityList() throws IOException{
       BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("../../resource/citylist.json").getFile()));
       Gson data = new Gson();
       citylist = data.fromJson(reader, City[].class);
-      for(City city : citylist){
+      for(City city : citylist) {
         city.setName(city.getName().toUpperCase());
       }
-    }
-    catch (IOException e){
-      e.printStackTrace();
-      citylist = null;
-    }
   }
-  private static void init(){
-    if(dummy == null)
-      dummy = new GetCityList();
+  private static void init() throws IOException{
+    if(citylist == null)
+      new GetCityList();
   }
-  public static ArrayList<City> byCityName(String cityname){
+  public static ArrayList<City> byCityName(String cityname) throws IOException{
     init();
     ArrayList<City> result = new ArrayList<City>();
     cityname = cityname.toUpperCase();
     for(City city : citylist){
-      if(city.getName().contains(cityname)) {
+      if(city.getName().contains(cityname)){
         result.add(new City(city));
       }
     }
