12dbaf4ae3f3341861da75484d5738c0fc81eaf3
Tony <buddy90_lost@yahoo.co.id>
2018-07-01 22:13:30 +0700

    change dirs

--- src/com/city/GetCityList.java
+++ src/com/city/GetCityList.java
@@ -1,21 +1,20 @@
-package com.data.city;
+package com.city;
 
-import com.functions.Functions;
+import com.data.CityData;
 import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 
 
 public class GetCityList {
-  static private City[] citylist = null;
+  static private CityData[] citylist = null;
   private GetCityList() throws IOException{
       BufferedReader reader = new BufferedReader(new FileReader(getClass().getResource("../../resource/citylist.json").getFile()));
       Gson data = new Gson();
-      citylist = data.fromJson(reader, City[].class);
-      for(City city : citylist) {
+      citylist = data.fromJson(reader, CityData[].class);
+      for(CityData city : citylist) {
         city.setName(city.getName().toUpperCase());
       }
   }
@@ -23,13 +22,13 @@ public class GetCityList {
     if(citylist == null)
       new GetCityList();
   }
-  public static ArrayList<City> byCityName(String cityname) throws IOException{
+  public static ArrayList<CityData> byCityName(String cityname) throws IOException{
     init();
-    ArrayList<City> result = new ArrayList<City>();
+    ArrayList<CityData> result = new ArrayList<CityData>();
     cityname = cityname.toUpperCase();
-    for(City city : citylist){
+    for(CityData city : citylist){
       if(city.getName().contains(cityname)){
-        result.add(new City(city));
+        result.add(new CityData(city));
       }
     }
     return result;
